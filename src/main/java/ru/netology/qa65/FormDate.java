bb682f392bc6863f1056b459112a4724d8513f61
gkz <giorgadzekonstantin@gmail.com>
2023-07-10 14:22:47 +0400

    Add FormDate

---
+++ src/main/java/ru/netology/qa65/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.qa65;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
--- src/main/java/ru/netology/qa65/Post.java
+++ src/main/java/ru/netology/qa65/Post.java
@@ -2,9 +2,9 @@ package ru.netology.qa65;
 
 public class Post {
     public String name;
-    public int passport;
+    public String passport;
     public String patronymic;
-    public int phone;
+    public String phone;
     public String surname;
     public boolean subscription;
 }
