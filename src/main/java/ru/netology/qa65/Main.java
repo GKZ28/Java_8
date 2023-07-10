8fb032143d5d734bb398fb5816c539f2debb08b0
gkz <giorgadzekonstantin@gmail.com>
2023-07-10 14:10:35 +0400

    Add Post

--- src/main/java/ru/netology/qa65/Main.java
+++ src/main/java/ru/netology/qa65/Main.java
@@ -2,6 +2,7 @@ package ru.netology.qa65;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Post post = new Post();
+
     }
 }
---
+++ src/main/java/ru/netology/qa65/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.qa65;
+
+public class Post {
+    public String name;
+    public int passport;
+    public String patronymic;
+    public int phone;
+    public String surname;
+    public boolean subscription;
+}
